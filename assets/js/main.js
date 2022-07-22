37a5684ea3d9220096bb4f4636c1b5fb52f2f9fd
Daniel-Hutanu <danielhutanu22@gmail.com>
2022-07-22 19:26:21 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+var ztxt = new Ztextify(".logo-text", {
+  depth: "25px",
+  layers: 15,
+  fade: true,
+  direction: "forwards",
+  event: "pointer",
+  eventRotation: "35deg"
+});
+var subLogo = new Ztextify(".sub-logo", {
+  depth: "10px",
+  layers: 5,
+  fade: true,
+  direction: "forwards",
+  event: "pointer",
+  eventRotation: "35deg"
+});
